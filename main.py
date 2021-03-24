71c24e2479f4d5759a659b7e4327c6e8d8168cc9
King321-mk4 <80783627+King321-mk4@users.noreply.github.com>
2021-03-24 13:42:46 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,10 @@
+from microbit import *
+# --------- = ---------
+# varibal = value (expression)
+
+answer = 42 
+hello_message = "hi there!" 
+
+basic.show_string(hello_message)   
+hello_message= "yo!"
+basic.show_string(hello_message)  
---
+++ main.ts
@@ -0,0 +1,7 @@
+//  --------- = ---------
+//  varibal = value (expression)
+let answer = 42
+let hello_message = "hi there!"
+basic.showString(hello_message)
+hello_message = "yo!"
+basic.showString(hello_message)
